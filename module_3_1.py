50cba4cf14e260405476a4c26e372f40d15b7c19
Srbin212 <srbin212@yandex.ru>
2024-09-14 10:03:54 +0300

    2023/10/11 00:00|Самостоятельная работа по уроку "Рекурсия"

--- module_3_1.py
+++ module_3_1.py
@@ -1,27 +1,23 @@
 calls = 0
 
-def count_calls(calls):
-    print(calls)
+
+def count_calls():
+    global calls
+    calls += 1
+
 
 def string_info(string):
-    global calls
-    calls += 1
-    new_tuple = (len(string), string.upper(), string.lower())
-    return new_tuple
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
 
 def is_contains(string, list_to_search):
-    global calls
-    calls += 1
-    for item in list_to_search:
-        new_list = []
-        new_list.append(item.lower())
-    if string.lower() in new_list:
-        return True
-    else:
-        return False
+    count_calls()
+    return string.upper() in [s.upper() for s in list_to_search]
+
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
 print(calls)
